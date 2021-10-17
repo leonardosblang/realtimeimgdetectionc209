a04813c9990f4a0c04e26551e80f81740bd09e66
Leonardo S.B. Lang <49520669+leonardosblang@users.noreply.github.com>
2021-10-17 20:22:24 -0300

    Refactored code

--- main.py
+++ main.py
@@ -1,62 +1,63 @@
-import cv2
+import cv2  # pip install opencv-python
 import numpy as np
 from mss import mss
 from PIL import Image
 import imutils
 
-sct = mss()
+aux = mss()
+
 while 1:
-	w, h = 1280, 720
-	monitor = {'top': 0, 'left': 0, 'width': w, 'height': h}
-	img = Image.frombytes('RGB', (w, h), sct.grab(monitor).rgb)
-	cv2.imshow('test', cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
+    weight, height = 1280, 720
+    monitor = {'top': 0, 'left': 0, 'width': weight, 'height': height}
 
-	img2 = np.uint8(img)
+    screenshot = Image.frombytes('RGB', (weight, height),
+                                 aux.grab(monitor).rgb)
 
-	template = cv2.imread('goomba.png')
-	template = template.astype(np.uint8)
-	template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-	template = cv2.Canny(template, 50, 200)
-	(tH, tW) = template.shape[:2]
+    image = np.uint8(screenshot)
 
+    # Seleciona a imagem
+    template = cv2.imread('goomba.png')
+    # Tipo de imagem do template
+    template = template.astype(np.uint8)
+    # Converte a cor p/ leitura
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    # Define a sensibilidade da detecção da borda
+    template = cv2.Canny(template, 50, 200)
+    # Altura e largura do template
+    (tH, tW) = template.shape[:2]
 
+    # Transforma a imagem em escala de cinza
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-	image = img2
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	found = None
-	for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-		resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
-		r = gray.shape[1] / float(resized.shape[1])
-		if resized.shape[0] < tH or resized.shape[1] < tW:
-			break
+    found = None
 
-		edged = cv2.Canny(resized, 50, 200)
-		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+    # Algoritmo p/ escalonamento de imagem
+    for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+        resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+        r = gray.shape[1] / float(resized.shape[1])
 
+        if resized.shape[0] < tH or resized.shape[1] < tW:
+            break
 
-		if found is None or maxVal > found[0]:
-			found = (maxVal, maxLoc, r)
+        edged = cv2.Canny(resized, 50, 200)
+        result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
 
-	(_, maxLoc, r) = found
-	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+        if found is None or maxVal > found[0]:
+            found = (maxVal, maxLoc, r)
 
-	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        # Pega a área que será envolvida pelo retângulo
+    (_, maxLoc, r) = found
+    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
 
-	a=2
+    # Traça o retângulo
+    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 
+    # Mostra a imagem
+    cv2.imshow('test', cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
 
-
-	cv2.imshow('test', cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
-	if cv2.waitKey(25) & 0xFF == ord('q'):
-		cv2.destroyAllWindows()
-		break
-
-
-
-
-
-
-
-
+    # Botão p/ fechar
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
